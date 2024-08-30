e7ea2540d5bb096b787129606d0350a8e04fec51
Alba <allbaluccia@gmail.com>
2024-08-30 18:13:31 +0000

    corrigiendo migraciones

--- src/api/models.py
+++ src/api/models.py
@@ -1,20 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 
-
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True, nullable=False)
-    lastname = db.Column(db.String(100), unique=True, nullable=False)
+    name = db.Column(db.String(100), unique=False, nullable=False)
+    lastname = db.Column(db.String(100), unique=False, nullable=False)
     birth_date = db.Column(db.Date, nullable=True)
     gender = db.Column(db.Enum('male', 'female', 'none', name='gender_enum'), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), unique=False, nullable=False)
-    usercalendar = relationship('Calendar', backref='user', lazy=True)
-    usersession = relationship('Session', backref='user', lazy=True)
-    
+    user_calendar = relationship('Calendar', backref='user', lazy=True)
+    user_session = relationship('Session', backref='user', lazy=True)
 
     def __repr__(self):
         return f'<User {self.name}>'
@@ -24,20 +22,20 @@ class User(db.Model):
             "id": self.id,
             "name": self.name,
             "lastname": self.lastname,
-            "birth_date": self.age,
+            "birth_date": self.birth_date,  
             "gender": self.gender,
             "email": self.email
-
         }
+
 class Psychologist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True, nullable=False)
-    lastname = db.Column(db.String(100), unique=True, nullable=False)
+    name = db.Column(db.String(100), unique=False, nullable=False)
+    lastname = db.Column(db.String(100), unique=False, nullable=False)
     experience = db.Column(db.String(100), nullable=False)
     language = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), unique=False, nullable=False)
-    psychologistCalendar = relationship('Calendar', backref='psychologist', lazy=True)
+    psychologist_calendar = relationship('Calendar', backref='psychologist', lazy=True)
 
     def __repr__(self):
         return f'<Psychologist {self.name}>'
@@ -47,17 +45,15 @@ class Psychologist(db.Model):
             "id": self.id,
             "name": self.name,
             "lastname": self.lastname,
-            "age": self.age,
             "experience": self.experience,
             "language": self.language,
             "email": self.email
-
         }
 
 class Manager(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True, nullable=False)
-    lastname = db.Column(db.String(100), unique=True, nullable=False)
+    name = db.Column(db.String(100), unique=False, nullable=False)
+    lastname = db.Column(db.String(100), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), unique=False, nullable=False)
 
@@ -65,33 +61,32 @@ class Manager(db.Model):
         return f'<Manager {self.email}>'
     
     def serialize(self):
-        return{
+        return {
             "id": self.id,
             "name": self.name,
             "lastname": self.lastname,
-            "email": self.email,
-            "password": self.password
+            "email": self.email
         }
 
 class Calendar(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     day = db.Column(db.Date, nullable=False)
-    month = db.Column(db.Date, nullable=False)
-    hour = db.Column(db.Date, nullable=False)
-    userCalendar = db.Column(db.Date, db.ForeignKey('user.id'), nullable=False)
-    psychologistCalendar = db.Column(db.Date, db.ForeignKey('psychologist.id'), nullable=False)
+    month = db.Column(db.Integer, nullable=False)  
+    hour = db.Column(db.Time, nullable=False)  
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  
+    psychologist_id = db.Column(db.Integer, db.ForeignKey('psychologist.id'), nullable=False)  
     
     def __repr__(self):
         return f'<Calendar {self.day}>'
     
     def serialize(self):
-        return{
+        return {
             "id": self.id,
             "day": self.day,
             "month": self.month,
             "hour": self.hour,
-            "userCalendar": self.userCalendar,
-            "psycologistCalendar": self.psychologistCalendar
+            "user_id": self.user_id,
+            "psychologist_id": self.psychologist_id
         }
 
 class Session(db.Model):
@@ -100,13 +95,13 @@ class Session(db.Model):
     fullSession = db.Column(db.Integer, unique=True, nullable=False)
     sessionPackage_5 = db.Column(db.Integer, unique=True, nullable=False)
     sessionPackage_10 = db.Column(db.Integer, unique=True, nullable=False)
-    usersession = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    usersession = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  
 
     def __repr__(self):
         return f'<Session {self.introductory}>'
     
     def serialize(self):
-        return{
+        return {
             "id": self.id,
             "introductory": self.introductory,
             "fullsession": self.fullSession,
@@ -124,12 +119,8 @@ class Chatbot(db.Model):
         return f'<Chatbot {self.id}>'
     
     def serialize(self):
-        return{
+        return {
             "id": self.id,
             "frequently_ask": self.frequently_ask,
             "relaxation_exercises": self.relaxation_exercises
         }
-
-    
-
-
