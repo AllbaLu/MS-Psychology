de749298db6f08652184568ad21b3f777fc5ec4b
Alba <allbaluccia@gmail.com>
2024-09-09 10:14:56 +0000

    problema de migraciones corregido

--- src/api/models.py
+++ src/api/models.py
@@ -7,16 +7,15 @@ from datetime import datetime
 db = SQLAlchemy()
 
 class User(db.Model):
-    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=False, nullable=False)
-    lastname = db.Column(db.String(100), unique=False, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
     birth_date = db.Column(db.Date, nullable=True)
     gender = db.Column(db.Enum('male', 'female', 'none', name='gender_enum'), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(255), unique=False, nullable=False)
-    user_calendar = relationship('Calendar', backref='user', lazy=True)
-    user_session = relationship('SessionCall', backref='user', lazy=True)
+    password = db.Column(db.String(255), nullable=False)
+    calendars = relationship('Calendar', backref='user', lazy=True)
+    sessions = relationship('SessionCall', backref='user', lazy=True)
 
     def __repr__(self):
         return f'<User {self.name}>'
@@ -33,13 +32,13 @@ class User(db.Model):
 
 class Psychologist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=False, nullable=False)
-    lastname = db.Column(db.String(100), unique=False, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
     experience = db.Column(db.String(100), nullable=False)
     language = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(255), unique=False, nullable=False)
-    psychologist_calendar = relationship('Calendar', backref='psychologist', lazy=True)
+    password = db.Column(db.String(255), nullable=False)
+    calendars = relationship('Calendar', backref='psychologist', lazy=True)
 
     def __repr__(self):
         return f'<Psychologist {self.name}>'
@@ -56,10 +55,10 @@ class Psychologist(db.Model):
 
 class Manager(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=False, nullable=False)
-    lastname = db.Column(db.String(100), unique=False, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(255), unique=False, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
     def __repr__(self):
         return f'<Manager {self.email}>'
@@ -95,11 +94,11 @@ class Calendar(db.Model):
 
 class SessionCall(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    introductory = db.Column(db.Integer, unique=False, nullable=False)
-    fullSession = db.Column(db.Integer, unique=False, nullable=False)
-    sessionPackage_5 = db.Column(db.Integer, unique=True, nullable=False)
-    sessionPackage_10 = db.Column(db.Integer, unique=True, nullable=False)
-    user_session = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  
+    introductory = db.Column(db.Integer, nullable=False)
+    full_session = db.Column(db.Integer, nullable=False)
+    session_package_5 = db.Column(db.Integer, nullable=False)
+    session_package_10 = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  
 
     def __repr__(self):
         return f'<Session {self.id}>'
@@ -108,10 +107,10 @@ class SessionCall(db.Model):
         return {
             "id": self.id,
             "introductory": self.introductory,
-            "fullsession": self.fullSession,
-            "sessionPackage_5": self.sessionPackage_5,
-            "sessionPackage_10": self.sessionPackage_10,
-            "usersession": self.user_session
+            "full_session": self.full_session,
+            "session_package_5": self.session_package_5,
+            "session_package_10": self.session_package_10,
+            "user_id": self.user_id
         }
 
 class Chatbot(db.Model):
