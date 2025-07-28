98c6454afe863e5a7cdd0fe03147689d9b761411
Alba <allbaluccia@gmail.com>
2025-07-28 13:10:44 +0000

    translate en/ home-about-interventions

--- src/front/js/i18n.js
+++ src/front/js/i18n.js
@@ -1,24 +1,16 @@
+// src/front/js/i18n.js
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-// Traducciones de ejemplo
+import translationEN from '../../locales/en/translation.json';
+import translationES from '../../locales/es/translation.json';
+import translationPT from '../../locales/pt/translation.json';
+
 const resources = {
-  en: {
-    translation: {
-      welcome: 'Welcome to my site',
-    },
-  },
-  pt: {
-    translation: {
-      welcome: 'Bem-vindo ao meu site',
-    },
-  },
-  es: {
-    translation: {
-      welcome: 'Bienvenido a mi sitio',
-    },
-  },
+  en: { translation: translationEN },
+  es: { translation: translationES },
+  pt: { translation: translationPT },
 };
 
 i18n
